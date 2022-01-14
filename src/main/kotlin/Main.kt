75b6c1e50f3620a55d1c4bb23af781f63842041a
PJ Wilt <p.wilt.jr@gmail.com>
2022-01-13 19:05:04 -0500

    Naive solution and naive solution with the intention of improvement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,37 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        for (i in 0 until nums.size - 1){
+            for (j in 0 until nums.size - 1) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+        return intArrayOf(-1)
+    }
+
+    fun twoSumButBetter(nums: IntArray, target: Int): IntArray {
+        //Work in progress
+        for (i in 0 until nums.size - 1){
+            for (j in 0 until nums.size - 1) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+
+        return intArrayOf(-1)
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
+    val example1: IntArray = intArrayOf(2, 7, 11, 15)
+    val example2: IntArray = intArrayOf(3, 2, 4)
+    val example3: IntArray = intArrayOf(3, 3)
+
+    println(solution.twoSum(example1, 9).contentToString())
+    println(solution.twoSum(example2, 6).contentToString())
+    println(solution.twoSum(example3, 6).contentToString())
 }
